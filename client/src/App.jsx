d4af2f06350c861912601652a4f263673be011d9
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-15 19:34:04 +0530

    gauresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () =>{
-    return (
-        <div>
-
-        </div>
-    )
+const App = () => {
+  return (
+    <div>
+        <h1>hello world</h1>
+    </div>
+  );
 }
 
 export default App
