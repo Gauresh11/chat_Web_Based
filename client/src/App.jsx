0577bd183a308d8b6661d8229a1b377a822bbe1b
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-15 19:48:18 +0530

    weg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import {StreamChat} from 'stream-chat';
-import{Chat}from 'stream-chat-react';
+import{ChannelListMessenger, Chat}from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+const apikey = '7c5ccd9kgwnb';
+const client = StreamChat.getInstance(apikey);
+
 const App = () => {
   return (
-    <div>
-        <h1>hello world</h1>
+    <div className="app__wrapper">
+          <Chat client={client} theme ="team light">
+                  <ChannelListMessenger
+                  
+                  />
+                  
+                  <ChannelListMessenger
+                  
+                  />
+
+
+          </Chat>
+        
     </div>
   );
 }
