dac05c8116b24102bb0e0ed99d19e8fd286bfd60
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-16 16:11:09 +0530

    dwa

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {StreamChat} from 'stream-chat';
 import{ChannelListMessenger, Chat}from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import {ChannelContainer, ChannelListContainer} from './components';
+import {ChannelContainer, ChannelListContainer,Auth} from './components';
 import './App.css';
 const apikey = '7c5ccd9kgwnb';
 const client = StreamChat.getInstance(apikey);
-
+const authToken = false;
 const App = () => {
   return (
+    if(!authToken) return <Auth />
     <div className="app__wrapper">
           <Chat client={client} theme ="team light">
              
