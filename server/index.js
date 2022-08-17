03c1cd7f251be2c161cd5b606537679c976bca68
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-17 11:11:34 +0530

    gauresh

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-awwad
+const express = require('express');
+const cors  = require('cors');
+const authRoutes = require("./routes/auth.js");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require('dotenv').config()
+app.use(express.json());
+app.use(express.urlencoded());
+app.get('/', (req,res) =>{
+    res.send("hello world!");
+});
+app.use('/auth', authRoutes);
+app.listen(PORT,()=> console.log(`Server Running on port ${PORT}`));
