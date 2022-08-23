1fe105d5b72f690aea49ba3c5896cf1f710edf65
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-23 21:58:17 +0530

    uh

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -8,7 +8,7 @@ const ListContainer = ({ children }) => {
         <div className="user-list__container">
             <div className="user-list__header">
                 <p>User</p>       
-                <p>Invite</p>
+                <p>Invite</p>   
             </div>
             {children}         
                     
