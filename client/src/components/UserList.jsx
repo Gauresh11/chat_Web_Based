3e4f6c4eeea5d1f0e2757e8d76ea0e4539a9f3e0
Gauresh11 <gaureshwadekar82@gmail.com>
2022-08-23 21:58:43 +0530

    kn

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -11,7 +11,7 @@ const ListContainer = ({ children }) => {
                 <p>Invite</p>   
             </div>     
             {children}         
-                    
+               
         </div>
     )
 }
